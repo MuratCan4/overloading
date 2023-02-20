581edd613f8582de4e4bff13b7ae6d73887caf22
Murat.Can <muratt.can.94@gmail.com>
2023-02-20 13:09:02 +0300

    OverLoading

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 22;
+            int b = 62;
+            string isim = "Murat";
+            Metotlar instance = new Metotlar();
+            instance.Topla(a,b,out int sonuc);
+            Console.WriteLine(sonuc);
+            instance.EkranaYazdir(a);
+            instance.EkranaYazdir(isim);
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla (int deger1, int deger2, out int toplam)
+        {
+            toplam = deger1 + deger2;
+        }
+        public void EkranaYazdir (int sayi)
+        {
+            Console.WriteLine(sayi);
+        }
+        public void EkranaYazdir (string sayi)
+        {
+            Console.WriteLine(sayi);
+        }
+    }
+}
